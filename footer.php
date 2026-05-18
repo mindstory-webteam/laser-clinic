f706d1962376dc7a9113ec07ca74f12fffc5cddd
Janavalsan-cms <janavalsan@mindstory.in>
2026-05-18 17:56:15 +0530

    reafactor : logo  update

--- footer.php
+++ footer.php
@@ -1,9 +1,11 @@
   <footer class="footer">
     <div class="container footer-grid">
       <div class="footer-main">
-        <div class="brand footer-brand"><a class="brand" href="/" aria-label="Laser Skin Clinic home">
-            <img class="brand-logo" src="assets/images/main-logo.png" alt="Laser Skin Hair & Cosmetic Clinic">
-          </a></div>
+        <div class="brand footer-brand">
+          <a class="brand" href="/" aria-label="Laser Skin Clinic home">
+            <img class="brand-logo" src="assets\images\main-logo.png" alt="Laser Skin Hair & Cosmetic Clinic">
+          </a>
+        </div>
         <p>Dr. Mili Raveendran, MD, DVD<br>Dermatologist & Cosmetic Dermato Surgeon<br>Reg. No. 29407</p>
         <p>Koorkenchery, Thrissur<br>☎ +91 487-244-1252<br>WhatsApp +91 99474 88474</p>
       </div>
