3cc583a892643a1490e144f6efc6fee99c903e7c
Janavalsan-cms <janavalsan@mindstory.in>
2026-05-18 17:01:33 +0530

    reafactor : favicon

--- footer.php
+++ footer.php
@@ -12,7 +12,8 @@
         <a href="/">Home</a>
         <a href="/?scroll=about" data-scroll="about">About</a>
         <a href="/treatments">Treatments</a>
-<a href="/?scroll=services" data-scroll="services">Services</a>        <a href="blogs.php">Blog</a>
+        <a href="/?scroll=services" data-scroll="services">Services</a>        
+        <a href="blogs.php">Blog</a>
         <a href="/?scroll=doctor" data-scroll="doctor">Doctor</a>
         <a href="/?scroll=contact" data-scroll="contact">Contact</a>
       </div>
