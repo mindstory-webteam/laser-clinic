29c5096f534c25f125112944730390fb048a3be4
Janavalsan-cms <janavalsan@mindstory.in>
2026-06-01 10:37:41 +0530

    feat : gtag

--- blog-details.php
+++ blog-details.php
@@ -12,6 +12,16 @@ if (!$currentBlog) {
 <html lang="en">
 
 <head>
+
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-T8WS7QXZ');</script>
+<!-- End Google Tag Manager -->
+
+
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
 
@@ -34,6 +44,13 @@ if (!$currentBlog) {
 </head>
 
 <body>
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T8WS7QXZ"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
+
   <!-- PRELOADER -->
   <div id="preloader">
     <img src="/assets/images/main-logo-inverse.webp" alt="Laser Skin Hair & Cosmetic Clinic" />
